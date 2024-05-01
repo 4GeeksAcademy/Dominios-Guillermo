b074eefbe93af4510b08a1dace01635a70c25e4f
Guillermo <guillermo.gil.cor@gmail.com>
2024-05-01 12:12:52 +0100

    new repo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "mine", "yours"];
+let adj = ["great", "big", "happy", "brave", "old"];
+let noun = ["jogger", "racoon", "pack", "mountain", "river"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let newArr = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// ejemplo1
+// for(let i =0;i<pronoun.length;i++){
+//   for(let n=0;n<adj.length;n++){
+//     for(let x=0;x<noun.length;x++){
+//       newArr.push(pronoun[i]+adj[n]+noun[x]+".com")
+//     }
+//   }
+// }
+// console.log(newArr)
+
+// ejemplo2
+// pronoun.forEach((element) =>
+//   adj.forEach((element1) =>
+//     noun.forEach((element2) =>
+//       newArr.push(element + element1 + element2 + ".com")
+//     )
+//   )
+// );
+// console.log(newArr);
+
+//ejemplo3
+newArr = pronoun
+  .map((element) =>
+    adj
+      .map((element1) =>
+        noun.map((element2) => element + element1 + element2 + ".com")
+      )
+  )
+  .flat(3);
+console.log(newArr);
